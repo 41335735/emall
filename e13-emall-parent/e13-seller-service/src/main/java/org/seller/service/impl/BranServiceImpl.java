4332b15840498fdd8e91d00b6491a454813b153a
Administrator <Administrator@hp-PC>
2019-04-09 16:00:54 +0800

    11

--- e13-emall-parent/e13-seller-service/src/main/java/org/seller/service/impl/BranServiceImpl.java
+++ e13-emall-parent/e13-seller-service/src/main/java/org/seller/service/impl/BranServiceImpl.java
@@ -47,7 +47,6 @@ public class BranServiceImpl implements BrandService {
 			}
 			
 		}
-		
 		PageHelper.startPage(pageNo, pageSize);//分页
 		Page<TbBrand> page =  (Page<TbBrand>) brandMapper.selectByExample(example);
 		Map<String,Object> result = new HashMap<String,Object>();
